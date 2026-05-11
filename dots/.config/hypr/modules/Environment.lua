60063d28edde82e3f325c5881757b3405399369e
Rahul <138906783+notcandy001@users.noreply.github.com>
2026-05-11 22:50:27 +0530

    Add Environment.lua module
    migration from .conf   to .lua

---
+++ dots/.config/hypr/modules/Environment.lua
@@ -0,0 +1,11 @@
+--  ┓┏┏┓┳┓┳┏┓┳┓┓ ┏┓┏┓
+--  ┃┃┣┫┣┫┃┣┫┣┫┃ ┣ ┗┓
+--  ┗┛┛┗┛┗┻┛┗┻┛┗┛┗┛┗┛
+ 
+--   
+
+
+-- See https://wiki.hypr.land/Configuring/Advanced-and-Cool/Environment-variables/
+
+hl.env("XCURSOR_THEME", "Bibata-Modern-Ice")
+hl.env("XCURSOR_SIZE",  "24")
