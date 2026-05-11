26ac10f54e015d47177ddb5a214a4653905675c3
Rahul <138906783+notcandy001@users.noreply.github.com>
2026-05-11 22:48:10 +0530

    Migrate hyprland.conf to hyprland.lua format
    migration from .conf   to .lua

---
+++ dots/.config/hypr/hyprland.lua
@@ -0,0 +1,48 @@
+--   ┓┏┓┏┏┓┳┓┓ ┏┓┳┓┳┓
+--   ┣┫┗┫┃┃┣┫┃ ┣┫┃┃┃┃
+--   ┛┗┗┛┣┛┛┗┗┛┛┗┛┗┻┛
+--                  
+-- Matugen colors
+require("colors")
+
+-- Monitors
+require("modules/moni")
+
+-- Programs
+require("modules/programes")
+
+-- Autostart
+require("modules/autostart")
+
+-- Environment variables
+require("modules/Environment")
+
+-- Permissions
+require("modules/permissions")
+
+-- Look and feel
+require("modules/looks")
+
+-- Animations
+require("modules/animations")
+
+-- Input
+require("modules/input")
+
+-- Keybindings
+require("modules/keybinds")
+
+-- Screenshot
+require("modules/screenshot")
+
+-- Window & layer rules
+require("modules/windowrules")
+
+
+
+
+
+
+
+
+
